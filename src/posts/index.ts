14527e6d4e41cd2d599bef40bbcdd40a4a03e0fb
Matthew Gibbons <matthew.gibbons@multiverse.io>
2023-01-27 14:32:58 +0000

    Added example of on-demand bulider function

--- src/posts/index.ts
+++ src/posts/index.ts
@@ -1,4 +1,11 @@
+// import { builder, type BuilderHandler } from "@netlify/functions";
 import ServerlessHttp from "serverless-http";
 import app from "./app.js";
 
+// Without On-Demand Builder
 export const handler = ServerlessHttp(app, { binary: ["image/*"] });
+
+// With On-Demand Builder
+// export const handler = builder(
+//   ServerlessHttp(app, { binary: ["image/*"] }) as unknown as BuilderHandler
+// );
